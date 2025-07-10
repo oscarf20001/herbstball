3e6e930d03c20382f53c4fdcfbd40f643efa9c4d
Oscar <altepommes04@gmail.com>
2025-07-10 16:44:21 +0200

    Login Form is now more specific about where to log-in

--- server/php/html-structure/loginForm.php
+++ server/php/html-structure/loginForm.php
@@ -1,5 +1,29 @@
+<?php
+// Zugriff auf die globale Variable
+$basePath = $_SERVER['DOCUMENT_ROOT']; // z. B. /Users/oscarstreich/httpdocs
+
+// Nur lokal bei Entwicklung anpassen:
+if (strpos($_SERVER['HTTP_HOST'], 'localhost') !== false || strpos($_SERVER['HTTP_HOST'], '127.0.0.1') !== false) {
+    $basePath .= '/Metis/herbstball_25';
+}
+
+require_once($basePath . '/server/php/html-structure/extract_part-URL.php');
+$outputURLEnding = getOutputURLEnding();
+$loginFormSubText = '';
+
+if($outputURLEnding == 'index'){
+    $loginFormSubText = '';
+}else if($outputURLEnding == 'einzahlung'){
+    $loginFormSubText = 'Einzahlungen';
+}else if($outputURLEnding == 'admin'){
+    $loginFormSubText = 'Adminpanel';
+}
+
+?>
+
 <div id="login-box">
     <h2>Login erforderlich</h2>
+    <sub><?php echo $loginFormSubText ?></sub>
     <?php if (isset($error)) echo "<p style='color:red;'>$error</p>"; ?>
     <form method="POST">
         <div class="input-field username">

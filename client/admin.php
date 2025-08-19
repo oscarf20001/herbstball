18536efec40fd2b319894da1ea2969043e3e11f9
Oscar <altepommes04@gmail.com>
2025-08-19 14:23:13 +0200

    Deleted redundand barStyle stylesheet

--- client/admin.php
+++ client/admin.php
@@ -38,7 +38,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['username'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Herbstball des MCG 2025 - Powered by Metis</title>
-    <link rel="stylesheet" href="<?= BASE_URL ?>/client/styles/barstyles.css">
     <link rel="stylesheet" href="<?= BASE_URL ?>/client/styles/barStyles.css">
     <link rel="stylesheet" href="<?= BASE_URL ?>/client/styles/einzahlungen.css">
     <link rel="stylesheet" href="<?= BASE_URL ?>/client/styles/inputFields.css">

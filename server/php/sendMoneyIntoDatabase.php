7aa2e0ad2cd1ec10e22f1bf67fa278c8765cf119
Oscar <altepommes04@gmail.com>
2025-09-12 16:12:04 +0200

    Added payment log

--- server/php/sendMoneyIntoDatabase.php
+++ server/php/sendMoneyIntoDatabase.php
@@ -1,4 +1,14 @@
 <?php
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    echo json_encode([
+        'status' => 'fail',
+        'message' => 'Kein eingeloggter User gefunden'
+    ]);
+    exit;
+}
+
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
@@ -20,6 +30,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     date_default_timezone_set('Europe/Berlin');
     $timestamp = date('Y-m-d H:i:s'); // Format für SQL: '2025-07-05 14:23:45'
 
+    // alten Geldbetrag holen (später für logging wichtig)
+    $oldCost = $conn->prepare("SELECT paid_charges FROM kaeufer WHERE person_id = ?");
+    $oldCost->bind_param('i', $id);
+    $oldCost->execute();
+    $oldCost->bind_result($paid_charges);
+    $oldCost->fetch();
+    $oldMoney = $paid_charges;
+    $oldCost->close();
+
     $stmt = $conn->prepare("UPDATE kaeufer SET paid_charges = paid_charges + ?, method = ?, d_paid = ? WHERE person_id = ?");
     $stmt->bind_param('dssi', $geld, $method, $timestamp, $id);
 
@@ -28,9 +47,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     ];
 
     if($stmt->execute()){
-        $response = [
-            'status' => 'success',
-        ];
+        // Logging des Payments
+        if(logPayment($conn, $id, $oldMoney, $geld)){
+            $response = [
+                'status' => 'success'
+            ];
+        }else{
+            echo json_encode($reponse);
+        }
     }
     $stmt->close();
 
@@ -41,4 +65,34 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Prüfen, ob die offene Summe gleich oder unter null ist
     $conn->close();
     echo json_encode($response);
+}
+
+function logPayment($conn, $kaeufer_id, $oldMoney, $newMoney){
+   $log = $conn->prepare("INSERT INTO payments (user_id, kaeufer_id, old_cost, added) VALUES (?,?,?,?)");
+
+    if (!$log) {
+        $response = [
+                'status' => 'fail',
+                'message' => 'Fehler beim Prepare: ' . $conn->error
+        ];
+        return false;
+    }
+
+    if (!$log->bind_param('iidd', $_SESSION['user_id'], $kaeufer_id, $oldMoney, $newMoney)) {
+        $response = [
+                'status' => 'fail',
+                'message' => 'Fehler beim Bind: ' . $log->error
+        ];
+        return false;
+    }
+
+    if (!$log->execute()) {
+        $response = [
+                'status' => 'fail',
+                'message' => 'Fehler beim Execute: ' . $log->error
+        ];
+        return false;
+    }
+    $log->close();
+    return true;
 }
